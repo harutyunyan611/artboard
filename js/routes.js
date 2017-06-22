59dcc19173a8e5e9bcb7873a7b7572aff254622b
harutyunyan611 <tsovakh@gmail.com>
2017-06-23 02:59:07 +0500

    bug fixes

--- js/routes.js
+++ js/routes.js
@@ -10,9 +10,11 @@ app.config(function($routeProvider) {
         
     })
     .when("/contacts", {  
-        templateUrl : "http://res.cloudinary.com/harutyunyan611/raw/upload/v1498168040/contact_k998wa.html"
+        templateUrl : "http://res.cloudinary.com/harutyunyan611/raw/upload/v1498168557/contact_agzr4a.html"
     })
-
+    .otherwise({
+        redirectTo: '/messages'
+    });
 });
 angular.module('protectme').config(function($sceDelegateProvider) {
   $sceDelegateProvider.resourceUrlWhitelist(['**']);
